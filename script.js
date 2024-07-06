44936a33ffb2c524b6e8d86330a488ab4ef42a60
Humberto <141511049+Humberto3560@users.noreply.github.com>
2024-07-06 07:57:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function toggleSection(sectionId) {
     const sections = document.querySelectorAll('.content-section');
     sections.forEach(section => {
         if (section.id === sectionId) {
-            section.style.display = section.style.display === 'block' ? 'none' : 'block';
+            section.style.display = section.style.display === 'none' || section.style.display === '' ? 'block' : 'none';
         } else {
             section.style.display = 'none';
         }
